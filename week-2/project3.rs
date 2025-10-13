a4cc4cb776bfd078a6a0e83643006a1b58dd244b
unknown <(chihurumnanya.chukwuemeka@pau.edu.ng)>
2025-10-13 00:36:22 -0700

    (My projects)

---
+++ week-2/project3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let p: f64 = 510000.0; 
+    let r: f64 = 5.0;      
+    let n: f64 = 3.0;      
+
+    let a = p * (1.0 - (r / 100.0)).powf(n);
+
+    println!("The value of the TV after {:.0} years is ₦{:.2}", n, a);
+}
