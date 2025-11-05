9db5438483ba8ca9fd66b29991fefb82c23e54ef
unknown <(chihurumnanya.chukwuemeka@pau.edu.ng)>
2025-11-05 19:15:17 +0100

    (Week-6 practice)

---
+++ week-6/practice_4/target/debug/deps/project_4.d
@@ -0,0 +1,5 @@
+C:\Users\Stanny\Documents\c.chukwuemekaCOS101\week-6\practice_4\target\debug\deps\project_4.d: src\main.rs
+
+C:\Users\Stanny\Documents\c.chukwuemekaCOS101\week-6\practice_4\target\debug\deps\project_4.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice_4/target/debug/project_4.d
@@ -0,0 +1,1 @@
+C:\Users\Stanny\Documents\c.chukwuemekaCOS101\week-6\practice_4\target\debug\project_4.exe: C:\Users\Stanny\Documents\c.chukwuemekaCOS101\week-6\practice_4\src\main.rs
