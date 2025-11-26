c221d613328653bb5df15151aa451e9a4d543ebd
unknown <(chihurumnanya.chukwuemeka@pau.edu.ng)>
2025-11-26 15:31:53 +0100

    delete a file

--- week-9/practice_3/src/main.rs
+++ week-9/practice_3/src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 
 fn main() {
-    
+
+    let _file = std::fs::File::create("data.txt").expect("create failed");
     fs::remove_file("data.txt").expect("could not remove file");
     println!("file is removed");    
 }
